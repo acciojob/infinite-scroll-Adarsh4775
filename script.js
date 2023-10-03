18956e541b8e1c2cf3e7fe30064c618b4158a46c
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-10-03 17:48:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
-//your code here!
 
+		document.addEventListener("DOMContentLoaded", function(){
+			const list = document.getElementById("infi-list");
+
+     function addItems(){
+		for(let i=1; i<=2; i++){
+		 const listitem = document.createElement("li");
+		 listitem.textContent=  "Item" +  (list.children.length+1);
+		 list.appendChild(listitem);
+	 }
+		}	
+			window.addEventListener("scroll", function(){
+				const scrollTop = document.documentElement.scrollTop;
+				const scrollHeight=document.documentElement.scrollHeight;
+				const clientHeight = document.documentElement.clientHeight;
+		        const buffer = 100;
+		if(scrollTop + clientHeight >= scrollHeight)
+		{
+			addItems();
+		}
+		});
+
+			addItems();
+		});
